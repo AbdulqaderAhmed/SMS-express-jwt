0e7efe96a2e4eb49e1f4db314fbc060102b82498
AbdulqaderAhmed <87320844+AbdulqaderAhmed@users.noreply.github.com>
2023-10-12 15:03:09 +0300

    Update parentRoute.js

--- src/routers/admin/parentRoute.js
+++ src/routers/admin/parentRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   register,
   updateParent,
+  Allparent
 } from "../../controllers/admin/parentController.js";
 import { tokenValidation } from "../../middlewares/tokenValidation.js";
 
@@ -9,5 +10,6 @@ export const parent = express.Router();
 
 parent.use(tokenValidation);
 
+parent.get('/parent', Allparent)
 parent.post("/create", register);
 parent.put("/:id", updateParent);
